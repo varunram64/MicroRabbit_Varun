77da9aee667478061a934a39b96b8d0e5dcae221
Varun Kalidindi <varunram64@gmail.com>
2021-03-30 10:18:24 +0530

    Implemented classes and interfaces for the Domain.Core.

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+
+namespace MicroRabbit.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime timeStamp { get; protected set; }
+
+        protected Command()
+        {
+            timeStamp = DateTime.Now;
+        }
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public DateTime timeStamp { get; protected set; }
+
+        protected Event()
+        {
+            timeStamp = DateTime.Now;
+        }
+    }
+}
